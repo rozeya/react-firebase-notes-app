13a91598a580d726e9641945dd7bbb63e30e3af4
Rose Carmen Espino <mrcespino@tip.edu.ph>
2026-04-01 09:16:33 +0800

    Update firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-apiKey: "AIzaSyAuzwhmjaHvNAwCGVEUK4qBEFxjNdRAq2A",
-  authDomain: "myfirstreact-7c215.firebaseapp.com",
-  projectId: "myfirstreact-7c215",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); // [cite: 263, 264, 265, 270, 271]
+export const db = getFirestore(app);
